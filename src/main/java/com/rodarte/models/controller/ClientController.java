00f1ea735358dd61dab00721c19fa4e6b63aa2a5
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-09-12 23:28:29 -0600

    Lectura 38: Uso de Cors para compartir recursos en API REST

--- src/main/java/com/rodarte/models/controller/ClientController.java
+++ src/main/java/com/rodarte/models/controller/ClientController.java
@@ -3,12 +3,16 @@ package com.rodarte.models.controller;
 import com.rodarte.models.entity.Client;
 import com.rodarte.models.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+// allow our angular app to access this resource; enabled http methods: GET, POST, PUT, DELETE
+// if not specified, it allows the selected origins to perform all operations
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 @RequestMapping("/api/clients")
 public class ClientController {
