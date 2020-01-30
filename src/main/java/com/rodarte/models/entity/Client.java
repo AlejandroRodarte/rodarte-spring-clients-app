cce10bf2d06637ed0b18f24c751756fd806113e8
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-01-30 13:52:52 -0700

    64. Anotaciones JavaBeans Validation en la clase Entity

--- src/main/java/com/rodarte/models/entity/Client.java
+++ src/main/java/com/rodarte/models/entity/Client.java
@@ -1,6 +1,9 @@
 package com.rodarte.models.entity;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -13,12 +16,17 @@ public class Client implements Serializable {
     @Column(name = "id")
     private Long id;
 
+    @NotEmpty
+    @Size(min = 4, max = 12)
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
+    @NotEmpty
     @Column(name = "last_name")
     private String lastName;
 
+    @NotEmpty
+    @Email
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
